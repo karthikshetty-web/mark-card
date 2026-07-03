30b39b4e330ce086bb428e7e37890d7e38aebf21
Karthikshetty-web <karthikshettymadelu@gmail.com>
2026-07-03 05:34:13 -0700

    Add dark mode feature to student marks calculator

--- script.js
+++ script.js
@@ -24,3 +24,17 @@ function getMarks(){
      Subject: ${subject}<br>
      Total Marks: ${total}`;
 }
+const themeBtn = document.getElementById("themeBtn");
+
+themeBtn.addEventListener("click", function () {
+
+    document.body.classList.toggle("dark");
+
+    if(document.body.classList.contains("dark")){
+        themeBtn.innerHTML = "☀️ Light Mode";
+    }
+    else{
+        themeBtn.innerHTML = "🌙 Dark Mode";
+    }
+
+});
